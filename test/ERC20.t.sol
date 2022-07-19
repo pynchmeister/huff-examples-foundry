743fe80c968676388b1b9d236cc4291a86e1ea96
Harris Levine <acyclicgraphs@gmail.com>
2022-07-19 10:24:37 -0400

    Begin foundry tests for huff examples (ERC20)

--- test/ERC20.t.sol
+++ test/ERC20.t.sol
@@ -5,22 +5,22 @@ import "foundry-huff/HuffDeployer.sol";
 import "forge-std/Test.sol";
 import "forge-std/console.sol";
 
-contract SimpleStoreTest is Test {
+contract ERC20Test is Test {
     /// @dev Address of the SimpleStore contract.  
-    SimpleStore public simpleStore;
+    ERC20 public erc20;
 
     /// @dev Setup the testing environment.
     function setUp() public {
-        simpleStore = SimpleStore(HuffDeployer.deploy("SimpleStore"));
+        erc20 = ERC20(HuffDeployer.deploy("ERC20"));
     }
 
     /// @dev Ensure that you can set and get the value.
-    function testSetAndGetValue(uint256 value) public {
-        simpleStore.setValue(value);
-        console.log(value);
-        console.log(simpleStore.getValue());
-        assertEq(value, simpleStore.getValue());
-    }
+    // function testSetAndGetValue(uint256 value) public {
+    //     simpleStore.setValue(value);
+    //     console.log(value);
+    //     console.log(simpleStore.getValue());
+    //     assertEq(value, simpleStore.getValue());
+    // }
 }
 
 interface SimpleStore {
